fb24f17e628e4241a837a49bf9e67afd925d3a80
Rohit Prajapati <93605069+Rohitjeevan@users.noreply.github.com>
2022-12-02 12:18:45 +0530

    ya

---
+++ script.js
@@ -0,0 +1,46 @@
+function opentab(tabname) {
+    for (tablink of tablinks) {
+      tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+      tabcontent.classList.remove("active-tab");
+    }
+  
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+  
+  }
+  function openmenu() {
+    sidemeu.style.right = "0";
+  }
+  
+  function closemenu() {
+    sidemeu.style.right = "-200px";
+  }
+
+
+var tablinks = document.getElementsByClassName('tab-links');
+var tabcontents = document.getElementsByClassName('tab-contents');
+
+
+
+var sidemeu = document.getElementById("sidemenu");
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxlnJ_0bYuaUurxVQ63pGylPPaOVLwE31W0bO1d4TNDcsIRHxUdsUxBbR4GGHfSHBbHgg/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+   const msg = document.getElementById("msg")
+   
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+      .then(response => {
+        msg.innerHTML = "Message sent Successfully ";
+       setTimeout(function (){
+          msg.innerHTML = "";
+       },5000)
+
+       form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+
